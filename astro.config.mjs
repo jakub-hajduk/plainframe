92f96b432296e6359596f4afecf397a418ea2674
Jakub Hajduk <devhajduk@gmail.com>
2026-01-02 13:34:55 +0100

    feat: add code highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
+import astroExpressiveCode from 'astro-expressive-code'
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: 'one-dark-pro'
-    },
-  },
+  integrations: [
+    astroExpressiveCode({
+      themes: ['one-dark-pro'],
+      styleOverrides: {
+        codeFontSize: 'var(--plainframe-size-regular-700)',
+        codeLineHeight: '1.8',
+        codeFontFamily: 'var(--plainframe-typography-code-700)',
+        codePaddingBlock: 'var(--plainframe-space-medium-900)',
+        codePaddingInline: 'var(--plainframe-space-medium-1300)',
+        borderRadius: 'var(--plainframe-radius-rectangle-300)',
+
+        frames: {
+          shadowColor: 'transparent'
+        }
+      }
+    })
+  ]
 });
