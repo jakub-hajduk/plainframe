4155304f93b38da8fb896ffe664aea8adc4cdb14
Jakub Hajduk <devhajduk@gmail.com>
2026-03-15 19:52:35 +0100

    chore(navigation): make navigation composable

--- packages/plainframe/src/partials/index.ts
+++ packages/plainframe/src/partials/index.ts
@@ -1,1 +1,4 @@
-export { default as Navigation } from './Navigation.astro';
+export { default as Navigation } from './navigation/Navigation.astro'
+export { default as NavigationItem } from './navigation/NavigationItem.astro'
+export { default as NavigationHeading } from './navigation/NavigationHeading.astro'
+export { default as NavigationDivider } from './navigation/NavigationDivider.astro'
